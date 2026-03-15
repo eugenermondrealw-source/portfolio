f5e2be415d616fb5fe43b803e49bec0a9455101e
Eugene <eugenermondrealw@gmail.com>
2026-03-15 19:56:23 +0800

    fix: configure conditional basePathfor Vercel and Github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.DEPLOY_TARGET === "github";
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export', // Enables static export
-  basePath: '/portfolio', // Replace with your actual repo name
+  output: isGithubPages ? "export" : undefined, // Static export only for GitHub Pages
+  basePath: isGithubPages ? "/portfolio" : "",   // Subpath only for GitHub Pages
+  assetPrefix: isGithubPages ? "/portfolio/" : "", // Asset prefix only for GitHub Pages
   images: {
     unoptimized: true, // Required for static export
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
+        protocol: "https",
+        hostname: "picsum.photos",
       },
     ],
   },
